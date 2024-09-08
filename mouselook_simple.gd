fb9a6f3adca01a7f63cd7e096f24de0ed31ad0c4
NGeren <ngerencser@complianceview360.com>
2024-09-07 22:58:56 -0400

    SPIN!

--- mouselook_simple.gd
+++ mouselook_simple.gd
@@ -1,3 +1,4 @@
+extends Camera3D
 @export var sensitivity: float = 0.2
 @export var invert_y: bool = false
 
@@ -14,7 +15,9 @@ func _ready():
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		_process_mouse_movement(event.relative)
-
+func _process(delta):
+	#self.transform.basis.y += Vector3(0, 100, 0);
+	self.rotate_y(1)
 func _process_mouse_movement(relative_motion: Vector2) -> void:
 	# Apply sensitivity and calculate rotation deltas
 	rotation_x -= relative_motion.y * sensitivity #* (if invert_y else -1)
@@ -24,12 +27,13 @@ func _process_mouse_movement(relative_motion: Vector2) -> void:
 	rotation_x = clamp(rotation_x, deg_to_rad(min_vertical_angle), deg_to_rad(max_vertical_angle))
 
 	# Apply rotation to the camera (y-axis rotation)
-	rotation_degrees.x = rad_to_deg(rotation_x)
-
+	#rotation_degrees.x = rad_to_deg(rotation_x)
+	#rotate_x(1)
+	self.transform.basis.y += Vector3(0, 100, 0);
 	# Apply rotation to the parent node (x-axis rotation)
-	if get_parent():
-		get_parent().rotate_y(deg_to_rad(rotation_y))
+	#if get_parent():
+	#	get_parent().rotate_y(deg_to_rad(rotation_y))
 
-func _input(event: InputEvent) -> void:
-	if event is InputEventKey and event.pressed and event.keycode == Key.ESCAPE:
-		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+#func _input(event: InputEvent) -> void:
+	#if event is InputEventKey and event.pressed and event.keycode == Key.ESCAPE:
+	#	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
